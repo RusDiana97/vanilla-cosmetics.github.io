56f54e00819c1cbb6c62b1e4c8f6e40d408c6d6b
Rus Diana <russ.dianav@gmail.com>
2023-01-04 19:27:58 +0200

    Create data.js

---
+++ assets/js/data.js
@@ -0,0 +1,123 @@
+// products data
+let productsData = [
+  {
+    id: "1",
+    name: "Treatment Masque",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 10,
+    image: "./assets/images/products-images/product-1.jpg",
+  },
+  {
+    id: "2",
+    name: "Facial Serum",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 20,
+    image: "./assets/images/products-images/product-2.jpg",
+  },
+  {
+    id: "3",
+    name: "Kevin.Murphy",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 50,
+    image: "./assets/images/products-images/product-3.jpg",
+  },
+  {
+    id: "4",
+    name: "I'm Fabulous",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 30,
+    image: "./assets/images/products-images/product-4.jpg",
+  },
+  {
+    id: "5",
+    name: "Glossier",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 25,
+    image: "./assets/images/products-images/product-5.jpg",
+  },
+  {
+    id: "6",
+    name: "Tatcha",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 15,
+    image: "./assets/images/products-images/product-6.jpg",
+  },
+  {
+    id: "7",
+    name: "Skindinavia Cosmetics",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 40,
+    image: "./assets/images/products-images/product-7.jpg",
+  },
+  {
+    id: "8",
+    name: "Uncover",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 35,
+    image: "./assets/images/products-images/product-8.jpg",
+  },
+  {
+    id: "9",
+    name: "Nostalgia",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 28,
+    image: "./assets/images/products-images/product-9.jpg",
+  },
+  {
+    id: "10",
+    name: "Daisy Marc Jacobs",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 50,
+    image: "./assets/images/products-images/product-10.jpg",
+  },
+  {
+    id: "11",
+    name: "Nostalgie",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 50,
+    image: "./assets/images/products-images/product-11.jpg",
+  },
+  {
+    id: "12",
+    name: "Dear Scrub",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 32,
+    image: "./assets/images/products-images/product-12.jpg",
+  },
+  {
+    id: "13",
+    name: "Argan Mood",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 40,
+    image: "./assets/images/products-images/product-13.jpg",
+  },
+  {
+    id: "14",
+    name: "Hemp Body Balm",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 29,
+    image: "./assets/images/products-images/product-14.jpg",
+  },
+  {
+    id: "15",
+    name: "Tea Tree",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, pariatur!",
+    price: 43,
+    image: "./assets/images/products-images/product-15.jpg",
+  },
+];
